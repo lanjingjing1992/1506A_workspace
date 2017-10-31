dda338b72cf8def358ff44767155eef78f8b2fb1
lanjingjing <2364365304@qq.com>
2017-10-31 16:30:42 +0800

    菜单练习

--- day02/test.py
+++ day02/test.py
@@ -98,6 +98,39 @@ class Test:
                 break
             else:
                 list.append(num)
+    def calclate5(self):
+        for i in range(1,101,1):
+            if i%5==0 and i%3!=0 and i%8!=0:
+                print i
+    def output7(self):
+        for i in range(100,1000,1):
+            #个位数
+            num=i%10
+            if num==7:
+                print i
+    def add(self):
+        for i in range(100,1000,1):
+            #百位
+            n=i//100
+            #十位
+            m=i%100//10
+            #个位
+            k=i%10
+
+            if n+m==k:
+                print i
+    def nextto(self):
+        for i in range(10,100,1):
+            #十位
+            n=i//10
+            #个位
+            m=i%10
+            if n-1==m or n+1==m:
+                print i
+    def print3(self):
+        for i in range(100,1000,1):
+            if i%100//10+i//100==3:
+                print i
 
 
 
@@ -109,10 +142,70 @@ class Test:
 
 
 
-c=Test()
+
+
+
+# c=Test()
 # c.calcMoney()
 # c.exactDivision()
 # c.inputNum()
 # c.decrease()
 # c.calc()
 # c.InputNumSort()
+# c.calclate5()
+# c.output7()
+# c.add()
+# c.nextto()
+# c.print3()
+
+
+class Customer:
+    def __init__(self):
+        self.count=0
+        self.name='admin'
+        self.pw='123'
+        self.map={1:['鱼香肉丝',16],2:['糖醋里脊',21],3:['酱牛肉',38],4:['老干妈炒饭',8]}
+    def order(self):
+        num=raw_input('如果是管理员的话请输入1，顾客的话输入其他')
+        if num=='1':
+            self.login()
+        else:
+            self.menu()
+
+    def login(self):
+        n=raw_input('请输入管理员姓名')
+        if n==self.name:
+            p=raw_input('请输入密码：')
+            if p==self.pw:
+                print 'welcome'
+                #添加菜品 价格
+                pass
+            else:
+                print '密码错误，请重新输入'
+                self.login()
+        else:
+            print '姓名错误，请重新输入'
+            self.login()
+    def menu(self):
+        print '菜品展示:\n' \
+              '编号        菜品名称        价格\n' \
+              '--------------------------------'
+        for i in range(1,len(self.map)+1,1):
+            print '%s         %s         %s'%(i,self.map[i][0],self.map[i][1])
+        print '--------------------------------'
+        id=int(raw_input('请根据菜单输入您所需菜品名称的编号'))
+        num=int(raw_input('请输入数量'))
+        sum=num*self.map[id][1]#小计
+        self.count+=sum
+        y_n=raw_input('是否继续点餐 是的话输入y，否则的话输入其他')
+        if y_n=='y':
+            self.menu()
+        else:
+            print '总计：   %s'%self.count
+
+
+
+
+c=Customer()#创建对象
+# c.login()#登陆
+c.order()
