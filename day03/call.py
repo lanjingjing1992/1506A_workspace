3403157fe4f009990313742317c6fca93529d4be
lanjingjing <2364365304@qq.com>
2017-11-01 11:28:38 +0800

    day03调用练习

---
+++ day03/call.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+from day03 import Test
+
+
+r=Test.Recursion()
+# print r.recursion5(5)
+# print r.rubbitnum(8)
+# sum=0.0
+# for i in range(1,21,1):
+#     sum+=float(r.calcmolecule(i)/r.calcdenominator(i))
+
+
+# print sum
+# num1=int(raw_input('请输入数字：'))
+# num2=int(raw_input('请输入共有几个数字相加'))
+# sum=0
+# for i in range(1,num2+1,1):
+#     sum+= r.calcAdd(num1,i)
+#
+#
+#
+# print sum
+r.listtest()
+
