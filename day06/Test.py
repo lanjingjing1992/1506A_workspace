06fe2f2ff63764d9bb12356dca785a2824dd4885
lanjingjing <2364365304@qq.com>
2017-11-04 10:31:38 +0800

    更多产品----糯米

--- day06/Test.py
+++ day06/Test.py
@@ -12,15 +12,24 @@ d.get('http://www.baidu.com')
 # d.back()
 # sleep(2)
 # d.close()#关闭
-set=d.find_element_by_xpath('//*[@id="u1"]/a[8]')#设置的元素
-ActionChains(d).click_and_hold(set).perform()
+# set=d.find_element_by_xpath('//*[@id="u1"]/a[8]')#设置的元素
+# ActionChains(d).click_and_hold(set).perform()
+# sleep(2)
+# d.find_element_by_xpath('//*[@id="wrapper"]/div[6]/a[1]').click()#点击搜索设置
+# sleep(2)
+# # d.close()
+# #保存设置
+# ths=d.find_elements_by_tag_name('th')
+# print type(ths)
+# for th in ths:#遍历
+#     print th.text
+# d.find_element_by_xpath('//*[@id="gxszButton"]/a[1]').click()
+# sleep(2)
+# alert=d.switch_to_alert()#弹出框
+# sleep(2)
+# alert.accept()#确定
+more=d.find_element_by_xpath('//*[@id="u1"]/a[9]')
+ActionChains(d).click_and_hold(more).perform()#点击更多产品
+d.find_element_by_link_text('糯米').click()#点击糯米
 sleep(2)
-d.find_element_by_xpath('//*[@id="wrapper"]/div[6]/a[1]').click()#点击搜索设置
-sleep(2)
-# d.close()
-#保存设置
-d.find_element_by_xpath('//*[@id="gxszButton"]/a[1]').click()
-sleep(2)
-alert=d.switch_to_alert()#弹出框
-sleep(2)
-alert.accept()#确定
+d.back()#回退
