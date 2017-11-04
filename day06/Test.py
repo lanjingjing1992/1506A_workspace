0f8f9f63fae4db769ff144fe9f36bb508ddc7c15
lanjingjing <2364365304@qq.com>
2017-11-04 13:36:32 +0800

    每一步骤都加了注释

--- day06/Test.py
+++ day06/Test.py
@@ -2,43 +2,54 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.action_chains import ActionChains#鼠标事件
+from selenium.webdriver.common.keys import Keys
 
 d=webdriver.Chrome()
 
 #打开百度网址
-d.get('http://mail.163.com/')
+d.get('https://i.qq.com/')
+#百度页面点击新闻
 # d.find_element_by_link_text('新闻').click()#点击新闻
 # sleep(2)
 # d.back()
 # sleep(2)
 # d.close()#关闭
+#百度页面点击设置
 # set=d.find_element_by_xpath('//*[@id="u1"]/a[8]')#设置的元素
 # ActionChains(d).click_and_hold(set).perform()
 # sleep(2)
-# d.find_element_by_xpath('//*[@id="wrapper"]/div[6]/a[1]').click()#点击搜索设置
+#点击搜索设置
+# d.find_element_by_xpath('//*[@id="wrapper"]/div[6]/a[1]').click()
 # sleep(2)
 # # d.close()
 # #保存设置
+#获取一组元素
 # ths=d.find_elements_by_tag_name('th')
 # print type(ths)
-# for th in ths:#遍历
+
+# for th in ths:#遍历元素
 #     print th.text
 # d.find_element_by_xpath('//*[@id="gxszButton"]/a[1]').click()
 # sleep(2)
+#获取弹出框
 # alert=d.switch_to_alert()#弹出框
 # sleep(2)
+#点击确定
 # alert.accept()#确定
 # more=d.find_element_by_xpath('//*[@id="u1"]/a[9]')
-# ActionChains(d).click_and_hold(more).perform()#点击更多产品
-# d.find_element_by_link_text('糯米').click()#点击糯米
+#点击更多产品
+# ActionChains(d).click_and_hold(more).perform()
+#点击糯米
+# d.find_element_by_link_text('糯米').click()
 # sleep(2)
 # d.back()#回退
-#邮箱账号登陆
-login=d.find_element_by_xpath('//*[@id="lbNormal"]')
-
-ActionChains(d).click_and_hold(login).perform()#鼠标事件
-sleep(2)
-d.switch_to_frame('x-URS-iframe')
-d.find_element_by_name('email').send_keys('15901337131')
-d.find_element_by_name('password').send_keys('123456')
-d.find_element_by_id('dologin').click()
+#邮箱账号登陆163邮箱
+# login=d.find_element_by_xpath('//*[@id="lbNormal"]')
+#
+# ActionChains(d).click_and_hold(login).perform()#鼠标事件
+# sleep(2)
+#进入到iframe层  就像一个箱子 里面是需要获取的元素 需要先切入到iframe里面
+# d.switch_to_frame('x-URS-iframe')
+# d.find_element_by_name('email').send_keys('15901337131')
+# d.find_element_by_name('password').send_keys('123456')
+# d.find_element_by_id('dologin').click()
