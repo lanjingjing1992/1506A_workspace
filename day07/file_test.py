fdd252871ea3a6d153abfadbc649ad5f7c62e605
lanjingjing <2364365304@qq.com>
2017-11-06 15:37:12 +0800

    day07查询lanjingjing表并且写入文件

--- day07/file_test.py
+++ day07/file_test.py
@@ -2,9 +2,10 @@
 class FIleIoTest:
     def inputMenu(self,str):
 
-        file=open('test.txt','w+')#以w+模式打开文件
+        file=open('test.txt','a+')#以w+模式打开文件
         file.write(str)#写入helloworld
-        file.seek(0,0)#把游标放到最前面
-        data=file.read()#读取三个字符
-        print data
+        # file.seek(0,0)#把游标放到最前面
+        # data=file.read()#读取三个字符
+        # print data
+        file.flush()
         file.close()#关闭文件
--- day07/mysql_Test.py
+++ day07/mysql_Test.py
@@ -1,14 +1,16 @@
 #coding=utf-8
 import pymysql
-
+from day07.file_test import FIleIoTest
 db=pymysql.connect(host='60.205.189.12',user='visitor',passwd='123456',port=3306)#连接数据库
 cursor=db.cursor()#用来进行操作数据库
-cursor.execute('show databases')
-datas= cursor.fetchall()#打印所有的数据
-for  i in datas:#遍历
-    print i
+# cursor.execute('show databases')
+# datas= cursor.fetchall()#打印所有的数据
+# for  i in datas:#遍历
+#     print i
 cursor.execute('use 1510f')
-cursor.execute('select * from animal_sell')
-datas= cursor.fetchall()#打印所有的数据
-for  i in datas:#遍历
-    print i
+cursor.execute('select * from lanjingjing')
+datas=cursor.fetchall()#获取全部数据
+for i in datas:
+    file=FIleIoTest()
+    print type(i)#元组类型
+    file.inputMenu(str(i))#转换成字符串
